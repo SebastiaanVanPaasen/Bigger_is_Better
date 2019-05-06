31121ff4df962376012561cbd41f47fd9a33e395
Sebastiaan <sebas.98vp@gmail.com>
2019-05-06 15:58:26 +0200

    Finalised wing loading diagram

--- inputs.py
+++ inputs.py
@@ -18,6 +18,9 @@ Rho_TO = 1.225  # kg/m^3    standard sea-level density
 Rho_Cruise = 0.6  # kg/m^3      estimated cruise density
 Rho_Landing = 1.225  # kg/m^3   standard sea-level density
 
+TOP = 4325  # guestimated
+Sigma_TO = Rho_TO / Rho_0  # ratio of densities
+
 V_stall_TO = 60  # m/s  guestimated
 V_stall_Cruise = 180 * kts_to_ms  # m/s   guestimated
 V_stall_Landing = np.sqrt(Landing_runway / 0.5847)  # m/s     based on runway length
@@ -26,21 +29,22 @@ CL_TO_max = 2.2  # guestimated
 CL_Cruise_max = 1.6  # guestimated
 CL_Landing_max = 2.55  # guestimated
 
-CL_TO = [(CL_TO_max / 1.21 - 0.2), (CL_TO_max / 1.21 - 0.1), CL_TO_max / 1.21, (CL_TO_max / 1.21 + 0.1),
-         (CL_TO_max / 1.21 + 0.2)]  # Estimated possible cl values
+CL_TO = [(CL_TO_max / 1.21 - 0.2), CL_TO_max / 1.21, (CL_TO_max / 1.21 + 0.2)]  # Estimated possible cl values
 CL_Landing = [(CL_Landing_max - 0.2), CL_Landing_max, (CL_Landing_max + 0.2)]  # guestimated
 
-TOP = 4325  # guestimated
-Sigma_TO = Rho_TO / Rho_0  # ratio of densities
+
 CD_0 = 0.018  # guestimated
 AR = [4.96, 6.96, 8.96]  # Estimated possible aspect ratio values
 V_cruise = 250.  # m/s      estimated cruise speed
 Oswald_factor = 0.76  # guestimated
+
 Climb_rate = 2000 * ftmin_to_ms  # m/s      set climb rate by CS25
 Climb_gradient = 0.03  # c/V set by CS25
 N_engines = 4  # guestimated
+
 Delta_CD_TO_gear_up = 0.015  # estimated from slides ADSEE-I, lecture 3
 Delta_CD_TO_gear_down = Delta_CD_TO_gear_up + 0.02  # estimated from slides ADSEE-I, lecture 3
 Delta_CD_Land = 0.085  # estimated from slides ADSEE-I, lecture 3
+
 Delta_oswald_TO = 0.05  # estimated from slides ADSEE-I, lecture 3
 Delta_oswald_Land = 0.1  # estimated from slides ADSEE-I, lecture 3
