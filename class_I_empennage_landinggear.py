a4945cb6910734571d92ec954d11c7eed22e7c42
Hidde4 <hidde4-@hotmail.com>
2019-05-13 14:42:45 +0200

    tail arms

--- class_I_empennage_landinggear.py
+++ class_I_empennage_landinggear.py
@@ -23,7 +23,19 @@ b = 30.  # wingspan [m]
 MLW = 1500000  # maximum landing weight [N]
 MTOW = 2000000  # maximum take-off weight [N]
 
+mass_frac_wing = 0.117
+mass_frac_emp = 0.023
+mass_frac_fuse = 0.098
+mass_frac_nac = 0.018
+mass_frac_prop = 0.072
+mass_frac_fix = 0.118
+m_fracs = [mass_frac_wing, mass_frac_emp, mass_frac_fuse, mass_frac_nac, mass_frac_prop, mass_frac_fix, 0.3, 0.3, 0.4]
 
+d_inner, d_outer, lcabin, lcabin_below, lcabin_above, l_tailcone_range, l_nosecone_range, l_fuselage, tot_seating_abreast, N_aisle_above, N_aisle_below, N_rows_above, N_rows_below, lpax_below, lpax_above, tot_seating_abreast_last_row, Pseat = fuselage_cross_section(
+    450, 450)
+xcg_payload = 0.5*l_fuselage
+xcg_fuel = 0.4*l_fuselage
+D_fuse = d_outer
 def class_I_empennage(m_fracs, MAC, l_fuselage, x_eng, l_n, xcg_OEW_MAC, xcg_payload, xcg_fuel, D_fuse, b):
     # Mass fractions main components, landing gear neglected 
     # m_fracswing = 0.117
@@ -83,19 +95,76 @@ def class_I_empennage(m_fracs, MAC, l_fuselage, x_eng, l_n, xcg_OEW_MAC, xcg_pay
     # Assumption: tail arms are taken from the cg of the tail
     # Horizontal and vertical tail locations
 
-    x_h = xcg_emp
-    x_v = xcg_emp
+    
+    
 
     # Normalized tail coefficients obtained from statistics
+    
+    
+    
+    
+    S = 300.
+    b = 50.
+    
+    x_h = xcg_emp
     V_h_norm = 0.99
+    tap_h            = 0.4
+    A_h              = 4.
+    sweep_quartchord_h = np.pi*30./180.
+    n = 1. 
+    print(x_h)
+    while n > 0.001:
+        
+        S_frac_h = (V_h_norm * MAC) / (x_h - xcg_aft)
+        S_h = S_frac_h*S
+        
+        b_h = np.sqrt(A*S_h)
+        C_r_h = (2.*S_h)/(b_h*(1.+tap_h))
+        C_t_h = C_r_h*tap_h
+        MAC_h = (2./3.)*C_r_h*((1.+tap_h+tap_h**2)/(1.+tap_h))
+        sweep_LE_h = np.arctan(np.tan(sweep_quartchord_h)-((C_r_h)/(2.*b_h))*(tap_h-1))
+        sweep_TE_h = np.arctan((C_t_h-C_r_h)/(b_h/2.)+np.tan(sweep_LE_h))
+        y_MAC_h = (b_h/6.)*((1.+2.*tap_h)/(1.+tap_h))
+        fuse_MAC_h = MAC_h-0.25*MAC_h-y_MAC_h*np.tan(sweep_TE_h)
+        
+        n = abs((x_h-(l_fuselage-fuse_MAC_h))/(x_h))
+        x_h = l_fuselage-fuse_MAC_h
+        
+    print(S_h, b_h, C_r_h, C_t_h, MAC_h, fuse_MAC_h, y_MAC_h)
+    print(x_h)
+    print()
+    x_v = xcg_emp    
     V_v_norm = 0.081
-
-    S_frac_h = (V_h_norm * MAC) / (x_h - xcg_aft)
-    S_frac_v = (V_v_norm * b) / (x_v - xcg_aft)
+    tap_v = 0.4
+    A_v = 1.5
+    sweep_LE_v = np.pi*35./180.
+    n = 1.
+    print(x_v)
+    while n > 0.001:
+        
+        S_frac_v = (V_v_norm * b) / (x_v - xcg_aft)
+        S_v = S_frac_v*S
+        
+        b_v = np.sqrt(A*S_v)
+        C_r_v = (S_v)/(b_v*(1.+tap_v))
+        C_t_v = C_r_v*tap_v
+        MAC_v = (2./3.)*C_r_v*((1.+tap_v+tap_v**2)/(1.+tap_v))
+        sweep_TE_v = np.arctan((C_t_v-C_r_v)/(b_v)+np.tan(sweep_LE_v))
+        y_MAC_v = (b_v/3.)*((1.+2.*tap_v)/(1.+tap_v))
+        fuse_MAC_v = MAC_v-0.25*MAC_v-y_MAC_v*np.tan(sweep_TE_v)
+        
+        n = abs((x_v-(l_fuselage-fuse_MAC_v))/(x_v))
+        x_v = l_fuselage-fuse_MAC_v
+    print(sweep_TE_v, S_v, b_v, C_r_v, C_t_v, MAC_v, fuse_MAC_v,y_MAC_v)
+    print(x_v)
+    print()
+    print(l_fuselage)
+    
     cg_locations = np.array([xcg_fuse, xcg_emp, xcg_fix, xcg_nac, xcg_prop, xcg_wing, xcg_fwd, xcg_aft, zcg])
 
     return cg_locations, S_frac_h, S_frac_v, X_LEMAC, x_h, x_v
 
+class_I_empennage(m_fracs, MAC, l_fuselage, x_eng, l_n, xcg_OEW_MAC, xcg_payload, xcg_fuel, D_fuse, b)
 
 def size_tail(wing_area, volume_fraction, tail_sweep, aspect_ratio):
     tail_area = volume_fraction * wing_area
