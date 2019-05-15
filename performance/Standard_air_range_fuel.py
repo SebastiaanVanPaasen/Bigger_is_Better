c34a7b8179689573f19b7a99132545cc85206d01
Nikki <nikkivanluijk@hotmail.com>
2019-05-15 10:06:19 +0200

    change

--- performance/Standard_air_range_fuel.py
+++ performance/Standard_air_range_fuel.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 14 08:55:23 2019
+Created on Mon May 13 11:45:46 2019
 
 @author: nikki
 """
@@ -11,23 +11,29 @@ import matplotlib.pyplot as plt
 
 #-------------------------------INPUTS-----------------------------------------
 """Weights"""
-#Wcr        =       Aircraft weight in N during Cruise
+#MTOW       =       Maximum take-off weight [N] (optional as fraction of W_TO)
+#MLW        =       Maximum landing weight [N] (optional as fraction of W_TO)
+#MZFW       =       Maximum zero fuel weight [N] (optional as fraction of W_TO)
+#OEW        =       Operational empty weight [N] (optional as fraction of W_TO)
+#MWP        =       Maximum payload weight [N] (optional as fraction of W_TO)
+#MFW        =       Maximum fuel weight [N] (optional as fraction of W_TO)
+#W_fr        = 0.05*W_f      #Reserve fuel weight as percentage of fuel weight
 
 """Aicraft configuration"""
 #CD0        =       Zero lift drag coefficient
 #A          =       Aspect ratio
-#e          =       Oswald efficiency factor
+#e          =       Oswald efficiency factor 
 #R_des      =       Design range [m]
-#S          =       Surface area m^2
+
 
 #------------------------STATISTICAL INPUTS----------------------------------
 
-Ct           = 12e-06      #Specific fuel conspumtion [kg/N/s] from B737MAX
+Ct           = 12e-06      #Specific fuel conspumtion [kg/N/s] from B737MAX 
 
 
 #------------------------------VERIFICATION DATA--------------------------------
 
-"""Inputs unit test based on B737 MAX 8"""
+"""Inputs unit test based on B737 MAX-8"""
 
 MTOW = 82190.*9.81
 OEW  = 45065*9.81
@@ -36,169 +42,123 @@ MZFW = 65952.* 9.81
 MFW  = 20826.*9.81          # Maximum fuel weight (including reserve fuel)
 W_fr = MFW/105. * 5.        #reserve fuel
 
-A = 9.
+A = 8.45
 e = 0.85
 CD0 = 0.020
+V = 236. #m/s
 g = 9.81
-S = 124.5
 
 R_range = 11000.  #range of x-axis
 R_des = 7000 #[km]
-Wcr = MLW #63000*9.81#assumption for now
-pax_max = 200
-n = 1 #load factor of numbe rof passengers
-
-
-"""Create reference line of B737 8 Max"""
-MPW1 = 20882*g
-MTOW1 = 82191*g
-OEW1 = 45070*g
-MFW1 = 31594*g
-
-Mcr = 0.79
-H_cr = 12000 #(m)
-S = 127 #m^2
-b = 35.92 #m
-A = b**2 / S
-
-#-----------------------------DEFINITIONS-------------------------------------
-#Standard air range (SAR) = distances travelled per mass fuel burned
-#Fuel burn is related to speed, altitude, thrust (or drag in steady flight)
-#unit of SAR (m/s)/(kg/s)
-
-def ISA_density(h):      # enter height in m
-    M = 0.0289644       #kg/mol molar mass of Earth's air
-    R = 8.3144590       #universal gas constant Nm/MolK
-
-    if h < 11000:
-        rho0 = 1.225   #kg/m^3
-        T = 288.15     #K
-        h0 = 0.         #m
-        a = -0.0065    #K/m
-        rho = rho0 * (T/(T + a*(h-h0)))**(1. + ((g*M)/(R*a)))
-
-    if h >= 11000:
-        rho0 = 0.36391 #kg/m^3
-        T = 216.65     #K
-        h0 = 11000.     #m
-        rho = rho0*np.e**((-g*M*(h-h0))/(R*T))
-
-    return rho
-
-def ISA_temp(h):
-    if h < 11000:
-        T = 288.15 - 0.0065*h   #in Kelvin
-        return T
-    if h >= 11000:
-        return 216.65    #in Kelvin
-
-def Mach(V,h):
-    gamma = 1.4
-    R = 287 #J/kg/K
-    a = np.sqrt(gamma*R*ISA_temp(h))
-    M = (V/3.6)/a
-    return M
-
-
-def SAR(h,A,S,e,CD0,Ct,Wcr):                 #enter V in m/s
-    V = np.linspace(600,1000,100)
-    SAR = []
-
-    for v in V:
-        k = 1./(np.pi*A*e)
-        q = 0.5*ISA_density(h)*(v/3.6)**2
-        SARi = 1./((v/3.6) / ( (CD0 + k *(Wcr/(q*S))**2) *q*S*Ct )) #in kg/m
-        SAR.append(SARi*1000.)   #in kg/km
-
-    return SAR,V
-
-def SAR_ref(h_cr,A,S,e,CD0,Ct,Wcr):                 #enter V in m/s
-    V = np.linspace(600,1000,100)
-    SAR = []
-
-    for v in V:
-        k = 1./(np.pi*A*e)
-        q = 0.5*ISA_density(h)*(v/3.6)**2
-        SARi = 1./((v/3.6) / ( (CD0 + k *(Wcr/(q*S))**2) *q*S*Ct )) #in kg/m
-        SAR.append(SARi*1000.)   #in kg/km
-
-    return SAR,V
-
-#------------------------------MAIN PROGRAM------------------------------------
-
-dh = 500                #step size in altitude
-H = range(7000,12500,dh)#altitude range
-
-
-min_SAR = []
-V_minSAR = []
-
-#For a given altitude (in def) run it for different speeds
-for h in H:
-    SAR_list = SAR(h,A,S,e,CD0,Ct,Wcr)[0]
-    V = SAR(h,A,S,e,CD0,Ct,Wcr)[1]
-
-    min_SAR.append(min(SAR_list))
-    i = SAR_list.index(min(SAR_list))
-    V_minSAR.append(V[i])
-
-
-    plt.subplot(221)
-    plt.plot(V,SAR_list,label='%s altitude [m]' % h)
-    #plt.hlines()
-    plt.title('Fuel consumption w.r.t. airspeed')
-    plt.xlabel("Airspeed [km/h]")
-    plt.ylabel("Fuel consumption [kg/km]")
-
-plt.legend()
-
-
-for j in range(len(min_SAR)):
-    plt.subplot(222)
-    plt.xlabel("Airspeed at minimum SAR [km/h]")
-    plt.ylabel("Minimum Fuel consumption [kg/km]")
-    plt.plot(V_minSAR[j],min_SAR[j],'o', label = '%s altitude [m]' % H[j])
-    plt.title('Minimum fuel consumption with corresponding airspeed and altitude')
-
-plt.legend()
-
-
-for j in range(len(min_SAR)):
-    V_minSAR[j] = Mach(V_minSAR[j],H[j])
-
-    plt.subplot(224)
-    plt.xlabel("Mach at minimum SAR ")
-    plt.ylabel("Minimum Fuel consumption [kg/km]")
-    plt.plot(V_minSAR[j],min_SAR[j],'o', label = '%s altitude [m]' % H[j])
-    plt.title('Minimum fuel consumption with corresponding Mach and altitude')
-
-
-#Fuel consumed per km per passenger/seat
-for h in H:
-    pax = pax_max*n
-    SAR_list = (SAR(h,A,S,e,CD0,Ct,Wcr)[0])
-    V = SAR(h,A,S,e,CD0,Ct,Wcr)[1]
-
-    for i in range(len(SAR_list)):
-        SAR_list[i] = SAR_list[i]/pax
-    plt.subplot(223)
-    plt.plot(V,SAR_list)#,label='%s altitude [m]' % h)
-    plt.title('Fuel consumption per passenger w.r.t. airspeed')
-    plt.xlabel("Airspeed [km/h]")
-    plt.ylabel("Fuel consumption [kg/km/passenger]")
-
-#plt.legend()
-plt.show()
-
-
-"""Once speed and altitude are selected, more precies SAR can be made 
-by taking into account the weight reduction due to fuel consumption"""
-
-#--------------------------------SENSITIVITY ANALYSIS-------------------------
-# print(V_minSAR)
-# print(min_SAR)
-# print(H)
 
 
 
+#------------------------------DEFINITIONS-----------------------------------
 
+def CL_CD(A,e,CD0):  #compute CL/CD at cruise
+    CL_CD = (3./4.)*np.sqrt((np.pi*A*e)/(3.*CD0))
+    return CL_CD
+    
+    
+def Wf_Wto():              #find V for which Wf/W_TO can be minimised
+    Wf_Wto_opt = []
+    V = []
+    for i in range(200,600):
+        V.append(i)
+        Wf_Wto = 1. - e**((-R_des*1000)/((i/Ct)*(CL_CD(A,e,CD0))))
+        Wf_Wto_opt.append(Wf_Wto)
+        
+    plt.plot(V,Wf_Wto_opt)
+    #plt.show()
+ 
+    
+def payload_range():
+    """Standard weight lines"""
+    plt.hlines(MTOW,0.,R_range,"g","--",label = "MTOW")
+    plt.hlines(OEW,0.,R_range,"r","--",label = "OEW")
+    plt.hlines(MZFW,0.,R_range,"b","--", label = "MZFW")
+    plt.hlines(MLW,0.,R_range,"y","--", label = "MLW")    
+    
+    
+    """Compute different ranges"""
+    #Finding harmonic range (max. payload, with fuel up to MTOW)
+    Wf1 = MTOW-MZFW-W_fr             #fuel weight at max. payload
+    R_harmonic = ((V/(g*Ct))*CL_CD(A,e,CD0)*np.log(MTOW/(MTOW-Wf1)))/1000.
+
+    #Max range line (increase fuel, decrease payload)
+    R_max = ((V/(g*Ct))*CL_CD(A,e,CD0)*np.log(MTOW/(MTOW-(MFW-W_fr))))/1000.
+    
+    #Ferry range (no payload all fuel, no MTOW anymore)
+    W_TO = OEW + MFW 
+    Wf2 =  MFW - W_fr 
+    R_ferry = ((V/(g*Ct))*CL_CD(A,e,CD0)*np.log(W_TO/(W_TO - Wf2)))/1000.
+    
+    plt.vlines(R_harmonic,OEW,MTOW,"m","--",label="Harmonic Range")
+    plt.vlines(R_max, OEW,MTOW,"crimson","--",label="Maximum Range")
+    plt.vlines(R_ferry,OEW,MTOW,"gray","--",label="Ferry Range")
+    plt.vlines(R_des,OEW,MTOW,"orange","--",label="Design Range")
+    
+    """Take-off weight line"""    
+    #TO line up to MTOW and R_harmonic 
+    TO = [MZFW+W_fr,MTOW]                     #additional fuel up to MTOW
+    range_TO = [0,R_harmonic]
+    
+    #TO line until max Range
+    TO.append(MTOW)
+    range_TO.append(R_max)
+    
+    #TO line until ferry range
+    TO.append(OEW+MFW)
+    range_TO.append(R_ferry)
+    
+    #Closing the diagram
+    TO.append(OEW)
+    range_TO.append(R_ferry)
+    
+    plt.plot(range_TO, TO,"navy",label = "TO weight")
+    
+
+    """Range line without reserve fuel"""
+    #Max payload line 
+    plt.hlines(MZFW,0.,R_harmonic,"k")   
+    
+    #Line up to Max range, increasing fuel, decreasing payload
+    weight = [MZFW, MTOW-MFW]              #weight line at max range
+    range_weight = [R_harmonic,R_max]
+    
+    #Line up to ferry range, all fuel, no payload
+    weight.append(OEW)            #weight line up to ferry range
+    range_weight.append(R_ferry)
+
+    plt.plot(range_weight,weight, "k", label = "Payload weight")
+    
+    """Range line including reserve fuel"""
+    #Max payload line 
+    plt.hlines(MZFW+W_fr,0.,R_harmonic,"gray")   
+    
+    for i in range(len(weight)):
+        weight[i] = weight[i] + W_fr
+
+    plt.plot(range_weight, weight, "gray",label = "With reserve fuel")    
+    
+    plt.title('Payload - range diagram')
+    plt.xlabel("Range [km]")
+    plt.ylabel("Weight [N]")
+    plt.legend()
+    plt.show()
+    #return weight, range_weight
+
+#a = (payload_range()[0][1]-payload())
+payload_range()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
