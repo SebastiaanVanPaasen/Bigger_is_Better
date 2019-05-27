ea4d1d2fce51dbab2a57122d27e2ad19e534273e
Mathilde <mathilde_terleth@hotmail.com>
2019-05-27 17:30:55 +0200

    change

--- structural analysis/Load_data.py
+++ structural analysis/Load_data.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 27 17:20:51 2019
+
+@author: mathi
+"""
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -28,10 +35,46 @@ def load(file_name):
     for i in range(len(data)-3):
         inputs.append(float(data[i][-1]))
         
-    return inputs
+    Mach = inputs[0]
+    cruise_alt = inputs[1]
+    W = inputs[2]
+    OEW = inputs[3]
+    Wing_W_frac = inputs[4] 
+    Fuselage_weight = inputs[5]
+    Empennage_weight =inputs[6] 
+    Nacelle_weight = inputs[7]
+    Engine_weight = inputs[8]
+    Landing_weight = inputs[9]
+    Fixed_equip_weight =inputs[10] 
+    Prop_weight =inputs[11]
+    Payload_weight = inputs[12]
+    Fuel_W_tot = inputs[13]
+    total_thrust = inputs[14]
+    Cl_cruise = inputs[15]
+    Cd_cruise= inputs[16]
+    rho = inputs[17] 
+    CD0 = inputs[18]
+    Lift_over_Drag = inputs[19]
+    V_cruise = inputs[20]
+    n_ult = inputs[21]
+    V = inputs[22]
+    b = inputs[23]
+    S = inputs[24]
+    Fuselage_length =inputs[25] 
+    Cr =inputs[26]
+    Sweep0 = inputs[27]
+    taper = inputs[28]
+    AR = inputs[29]
+    e = inputs[30]
+    Wing_W = inputs[31]
+    n_engines = inputs[32]
+    engine_weigth = inputs[33]
     
-#inputs = load('aerodynamic_concept')
-#print (inputs)
+    
+    return Mach,cruise_alt,W,OEW,Wing_W_frac,Fuselage_weight,Empennage_weight,Nacelle_weight ,Engine_weight,Landing_weight ,Fixed_equip_weight ,Prop_weight,Payload_weight,Fuel_W_tot,total_thrust,Cl_cruise,Cd_cruise,rho,CD0,Lift_over_Drag,V_cruise,n_ult,V,b,S,Fuselage_length,Cr,Sweep0,taper,AR,e,Wing_W,n_engines,engine_weigth 
+    
+inputs = load('aerodynamic_concept')
+print (inputs)
 
 #file_name = 'aerodynamic_concept'
 #
