d1376a895f53947a4c5086784627579d3e3015a6
Mathilde <mathilde_terleth@hotmail.com>
2019-05-29 17:15:47 +0200

    change

--- read_csv_input.py
+++ read_csv_input.py
@@ -39,7 +39,8 @@ import sys
 
 
 def read_output(design):
-    sys.path.append("C:/Users/Hidde/Documents/Aerospace Engineering Bachelor Year 3/Design Synthesis Exercise/Bigger_is_Better")
+#    sys.path.append("C:/Users/Hidde/Documents/Aerospace Engineering Bachelor Year 3/Design Synthesis Exercise/Bigger_is_Better")
+    sys.path.append("C:/Users/mathi/Documents/DSE/Bigger_is_Better")
     
 #    ROOT_DIR = os.path.dirname(os.path.abspath("excel_results"))
     input_path = sys.path[-1] + '\excel_results'
--- structural analysis/loading_and_moment_diagrams.py
+++ structural analysis/loading_and_moment_diagrams.py
@@ -127,7 +127,7 @@ def Loadcalculator(x0,Ff):
         strutreachedyet = True
     else:
         strutreachedyet = False
-        
+#        
     #print(xmiddlevalues)
     for i, x in enumerate(xmiddlevalues):
         ### Geometry calculations
