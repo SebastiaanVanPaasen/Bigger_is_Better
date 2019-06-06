ae49a5338e3e2e7939bfbd8cdc1301ec201f36cc
Nikki <nikkivanluijk@hotmail.com>
2019-06-06 10:52:43 +0200

    TO and landing distances update

--- Detailed_design_tools/Performance_tools/Runway_distances.py
+++ Detailed_design_tools/Performance_tools/Runway_distances.py
@@ -17,11 +17,13 @@ W_TO       =       63502.9318 *9.81      #MTOW   #Weight at take-off [N]
 W_land     =       58967.0081  *9.81    #65310*9.81 #Maximum landing weight [N]
 
 T_TO       =       96.3*1000*2#Total static thrust of all engines at take-off [N]
+T0         =       96.3*1000*2 #127.62*1000  #@ SEA LEVEL!!!
 
 CL_maxto   =       2.2 
 CL_max_land=       3.2
 CD_to      =       0.05 
 CD_land    =       0.09
+CD_TO      =       0.07
 
 A          =       9.44       #Aspect ratio [-]
 e          =       0.85       #Oswald efficiency factor [-]
@@ -116,7 +118,28 @@ def TO_distance(W_TO,S,rho,CL_maxto,bypass,T_TO,A): #Required distance to pass s
     theta = np.arccos(1- (h_to/R))
     sa = R*np.sin(theta)
     
-    return S_to,sa+sg
+    #Method 3: Flight mechanics course
+    h_to = 10.7
+    gamma = 3*np.pi/180  #climb angle
+    
+    Vs_TO = np.sqrt((W_TO*2)/(rho*S*CL_maxto))
+    V_LOF = 1.05*Vs_TO
+    CL_to = mu*np.pi*A*e
+    
+    T_mean = 0.75*((5. + bypass)/(4.+ bypass))*T_TO  #quite low      
+    D_mean = 0.5*rho*(V_LOF/np.sqrt(2))**2*S*CD_TO
+    L_mean =  0.5*rho*(V_LOF/np.sqrt(2))**2*S*CL_to
+    
+    Dg_mean = mu*(W_TO - L_mean)
+    a_mean = (g/W_TO)*(T_mean - D_mean - Dg_mean)
+    S_ground = V_LOF**2 / (2*a_mean)
+    
+    S_climb = (h_to - (1-np.cos(gamma))*(V_LOF**2/(0.15*g)))/ np.tan(gamma)
+    S_trans = (V_LOF**2/(0.15*g))*np.sin(gamma)
+    
+    S_tot = S_ground + S_climb + S_trans
+    
+    return S_to,sa+sg,S_tot
     
  
 
@@ -164,7 +187,6 @@ def BFL(A,e,T_TO,W_TO,CD_to, CL_maxto, bypass,rho,g):      #Balanced field lengt
     #Constants from torenbeek
     h_to = 10.7 #[m]
     gamma2_min = 0.024
-    mu = 0.02
     
     #Compute variables
     CL_to = mu*np.pi*A*e    
@@ -176,7 +198,7 @@ def BFL(A,e,T_TO,W_TO,CD_to, CL_maxto, bypass,rho,g):      #Balanced field lengt
     #Find overall equation
     a = 0.863/(1. + 2.3*dgamma2)    
     b = ((W_TO/S)/(rho*g*CL2)) + h_to    
-    c = (1./(T_mean/W_TO - mu_dash)) + 2.7
+    c = (1./(T_mean/W_TO - mu_dash)) + 4.6
     
     BFL = a*b*c
     return BFL
@@ -191,7 +213,7 @@ def BFL(A,e,T_TO,W_TO,CD_to, CL_maxto, bypass,rho,g):      #Balanced field lengt
 #           =       0.40 - 0.50 thrustreversers and spoilers
 #           =       0.50 - 0.60 + nose wheel braking
 
-def S_land(g,W_land,S,rho,CL_max_land):
+def S_land(T0,g,W_land,S,rho,CL_max_land,CD_land):
     #Method 1 from torenbeek
     #Constants from torenbeek
     h_land = 15.3
@@ -231,7 +253,24 @@ def S_land(g,W_land,S,rho,CL_max_land):
     Ja = (ISA_density(0)/(2*(W_TO/S)))*(0.02 + dCD0 + (k1 + (G/(np.pi*A*e))*CL_max_land**2) - mu*CL_max_land)
     
     sg = 1.5*Vtd + (1/(2*9.81*Ja))*np.log(1 + (Ja/Jt)*Vtd**2)
-    return SL, sa+sf+sg
+    
+    #Method 3: Anderson
+    Vs_land = np.sqrt((2*W_land)/(rho*S*CL_max_land))
+    Vap = 1.3*Vs_land
+    
+    S_trans = 2.6*Vs_land
+    
+    gamma = 2.3*np.pi/180
+    R = 1.3**2 * (((W_land/S)*(2/rho)*(1/CL_max_land))/(0.1*g))
+    S_air = R*np.sin(gamma) + ((h_land - (1 - np.cos(gamma))*R)/np.tan(gamma))
+    
+    T_mean_rev = 0.25*((5. + bypass)/(4.+ bypass))*T0        
+    D_mean = 0.5*rho*(Vap/np.sqrt(2))**2*S*CD_land
+    L_mean =  0.5*rho*(Vap/np.sqrt(2))**2*S*CL_max_land*0.8
+    
+    S_brake = (W_land**2/(2*g*S)*(2/rho)*(1.3**2/CL_max_land**2)*(1. / (T_mean_rev + D_mean + mu*(W_land - L_mean))))
+    
+    return SL,sa+sf+sg, S_trans+S_air+S_brake
 
 
 
@@ -248,8 +287,16 @@ S_TO_fail = TO_eng_fail(W_TO,g,S,rho,CL_maxto,A,e,T_TO,CD_to,Vx)
 #Balenced field length
 BFL = BFL(A,e,T_TO,W_TO,CD_to, CL_maxto, bypass,rho,g)
 
+S_land = S_land(T0,g,W_TO,S,rho,CL_max_land,CD_land)
 
-S_land = S_land(g,W_TO,S,rho,CL_max_land)
+#Regulations according to flight mechanics
+req_land = (10/6)*max(S_land)
+
+if BFL > max(S_TO):
+    req_TO = BFL
+elif max(S_TO) > BFL:
+    req_TO = max(S_TO)*1.15
+
 
 #plt.hlines(S_land,Vx[0],Vx[-1],'k','--',label = "landing")
 plt.hlines(BFL, Vx[0],Vx[-1],"gray","--",label = "BFL")
@@ -261,10 +308,12 @@ plt.title('Balenced field length (engine failure)')
 plt.legend()
 plt.show()    
     
-print (" Standard TO length:" , S_TO, "m")
-print (" Standard landing length:", S_land,"m")    
-print ( " Balanced field length:", BFL,"m")
-    
+print ("Standard TO length:" , S_TO, "m")
+print ("Standard landing length:", S_land,"m")    
+print ("Balanced field length:", BFL,"m")
+print ()
+print ("Required TO field length:", req_TO, "m")
+print ("Required landing field length:", req_land, "m")
     
     
     
