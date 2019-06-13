70f0e25ad23d50632a4a213ad32c9a408b270e92
Unknown <sebas.98vp@gmail.com>
2019-06-13 17:35:17 +0200

    connected files to practised_2

--- structural analysis/parameter_requirements.py
+++ structural analysis/parameter_requirements.py
@@ -1,40 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 28 09:19:12 2019
-
-@author: mathi
-"""
-
-import loading_and_moment_diagrams as lm
-from loading_and_moment_diagrams import load_diagrams
-from centroid_wing import wing_centroid
 import centroid_wing as cw
 import numpy as np
+from airfoil_geometry import airfoil_geometry
 
-z_centroid_all_sec, y_centroid_all_sec, y_loc_spar_up, y_loc_spar_low, y_loc_stiff_up, y_loc_stiff_low, y_vertical_spar, z_loc_stiff_up, spar_loc_sec,z_loc_stiff_low = wing_centroid(cw.boom_area, cw.spar_areas_hori, cw.t_spar_v, cw.z_c_airfoil, cw.y_c_airfoil, cw.n_stiff_up, cw.n_stiff_low, cw.HalfspanValues)
-data_y_all_sec_up = cw.data_y_upper_all_sec
-data_y_all_sec_low = cw.data_y_lower_all_sec
-Mz = load_diagrams(100)[0]
+#Mz = load_diagrams(100)[0]
 
-def required_Izz(HalfspanValues, data_y_all_sec, y_centroid_all_sec, Mz):
-    
+def required_Izz(N, b, c, Mz):
+    HalfspanValues = np.linspace(0, b / 2 - 0.00001, N)
+    data_y_all_sec_up = airfoil_geometry(N, b, c)[1]
+    data_y_all_sec_low = airfoil_geometry(N, b, c)[2]
+
+    airfoil_area, z_c_airfoil, y_c_airfoil = cw.get_skin_centroid(N, b, c)
+    z_centroid_all_sec, y_centroid_all_sec, y_loc_spar_up, y_loc_spar_low, y_loc_stiff_up, y_loc_stiff_low, y_vertical_spar, z_loc_stiff_up, spar_loc_sec,z_loc_stiff_low = cw.wing_centroid(cw.boom_area, cw.spar_areas_hori, cw.t_spar_v, z_c_airfoil, y_c_airfoil, cw.n_stiff_up, cw.n_stiff_low, N, b, c)
+
     I_zz_req_all_sec = np.zeros((len(HalfspanValues), 1))
     sigma_ult = 110 * 10 ** 6
     
     for i in range(len(HalfspanValues)):
         y_up_max = -max(data_y_all_sec_up[i]) - (-1)*y_centroid_all_sec[i]
         y_low_max = -min(data_y_all_sec_low[i]) - (-1)*y_centroid_all_sec[i]
-        print(y_low_max)
+#        print(y_low_max)
         y_max = max(abs(y_up_max),abs(y_low_max))
         I_zz_req_all_sec[i] = abs(Mz[i]) * y_max / sigma_ult
 #        print(y_max)
     return I_zz_req_all_sec
 
  #8.54#7.11#6.63#6.06
-print("req Izz",required_Izz(cw.HalfspanValues, data_y_all_sec, y_centroid_all_sec, Mz))
-#print("req Izz",required_Izz(5.8451))
-
-strut_length = 20.95
+#print("req Izz",required_Izz(cw.HalfspanValues, data_y_all_sec, y_centroid_all_sec, Mz))
+##print("req Izz",required_Izz(5.8451))
+#
+#strut_length = 20.95
 
 #def required_strut_area(strut_length):
 #    angle = (20.78/180)*np.pi
--- structural analysis/spar_locations.py
+++ structural analysis/spar_locations.py
@@ -5,27 +5,21 @@ Created on Mon Jun 10 20:14:52 2019
 @author: mathi
 """
 import numpy as np
-from loading_and_moment_diagrams import c
-import loading_and_moment_diagrams as lm
-
-N= 100
-b = lm.b
-HalfspanValues = np.linspace(0, b / 2 - 0.00001, N)
 
 nr_spars = 4
 first_spar = 0.2
 last_spar = 0.6
 
-def spar_loc(HalfspanValues, nr_spars, first_spar, last_spar):
-    
+def spar_loc(N, b, nr_spars, first_spar, last_spar, c):
+    HalfspanValues = np.linspace(0, b / 2 - 0.00001, N)
     spar_loc_sec = []
     for i in range(len(HalfspanValues)):
         first_spar_location = first_spar*c(HalfspanValues[i])
         last_spar_location = last_spar*c(HalfspanValues[i])
         delta_spar = (last_spar_location-first_spar_location)/(nr_spars-1)
-        spar_loc = np.arange(first_spar_location,last_spar_location+delta_spar,delta_spar)
+        spar_loc = np.arange(first_spar_location,last_spar_location+delta_spar-0.001,delta_spar)
         spar_loc_sec.append(spar_loc)
         
     return spar_loc_sec, delta_spar
 
-spar_loc_sec, delta_spar = spar_loc(HalfspanValues, nr_spars, first_spar, last_spar)
+#spar_loc_sec, delta_spar = spar_loc(HalfspanValues, nr_spars, first_spar, last_spar)
